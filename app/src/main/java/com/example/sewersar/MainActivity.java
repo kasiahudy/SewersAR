12a46e8bc453777b9a56cfab7e0a63e8f192da3e
Kasia <k.hudy96@gmail.com>
2020-04-12 12:41:27 +0200

    Added location listener

--- app/src/main/java/com/example/sewersar/MainActivity.java
+++ app/src/main/java/com/example/sewersar/MainActivity.java
@@ -1,41 +1,54 @@
 package com.example.sewersar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import uk.co.appoly.arcorelocation.LocationMarker;
+import uk.co.appoly.arcorelocation.LocationScene;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import  android.app.Activity;
 import  android.app.ActivityManager;
 import 	android.os.Build;
 import 	android.util.Log;
+import android.view.MotionEvent;
 import 	android.widget.Toast;
 import 	android.content.Context;
 
+import com.google.ar.core.Anchor;
 import com.google.ar.core.Frame;
-import com.google.ar.core.Pose;
-import com.google.ar.core.Session;
-import com.google.ar.core.TrackingState;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
 import  com.google.ar.sceneform.rendering.ModelRenderable;
 import  com.google.ar.sceneform.ux.ArFragment;
-import  com.google.ar.sceneform.ux.TransformableNode;
 import 	android.net.Uri;
 import 	android.view.Gravity;
-import  com.google.ar.core.Anchor;
-import  com.google.ar.core.HitResult;
-import  com.google.ar.core.Plane;
-import  com.google.ar.sceneform.AnchorNode;
-import android.view.MotionEvent;
 import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.ux.TransformableNode;
 
-import java.util.Iterator;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
+    private final int REQUEST_PERMISSION_LOCATION=1;
 
-    ArFragment arFragment;
-    ModelRenderable lampPostRenderable;
+    private ArFragment arFragment;
+    private ModelRenderable lampPostRenderable;
+    private ModelRenderable andyRenderable;
+
+    private LocationScene locationScene;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     @SuppressWarnings({
@@ -46,8 +59,15 @@ public class MainActivity extends AppCompatActivity {
         if (!checkIsSupportedDeviceOrFinish(this)) {
             return;
         }
+
+
+
+
         setContentView(R.layout.activity_main);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        ArSceneView arSceneView = arFragment.getArSceneView();
+        Frame arFrame = arSceneView.getArFrame();
+
         ModelRenderable.builder()
                 .setSource(this, Uri.parse("Pipe.sfb"))
                 .build()
@@ -60,22 +80,90 @@ public class MainActivity extends AppCompatActivity {
                     return null;
                 });
 
+        CompletableFuture<ModelRenderable> andy = ModelRenderable.builder()
+                .setSource(this, R.raw.andy)
+                .build();
+
+
+        CompletableFuture.allOf(andy)
+                .handle(
+                        (notUsed, throwable) ->
+                        {
+                            if (throwable != null) {
+                                //DemoUtils.displayError(this, "Unable to load renderables", throwable);
+                                return null;
+                            }
+
+                            try {
+                                andyRenderable = andy.get();
+
+                            } catch (InterruptedException | ExecutionException ex) {
+                                //DemoUtils.displayError(this, "Unable to load renderables", ex);
+                            }
+                            return null;
+                        });
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new MyLocationListener(this,locationScene, andyRenderable, arSceneView, arFrame);
+        if(ActivityCompat
+                .checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_LOCATION);
+        }
+
         /*arFragment.setOnTapArPlaneListener(
-                (HitResult hitresult, Plane plane, MotionEvent motionevent) -> {
-                    if (lampPostRenderable == null){
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (andyRenderable == null) {
                         return;
                     }
-                    Anchor anchor = hitresult.createAnchor();
+
+                    // Create the Anchor.
+                    Anchor anchor = hitResult.createAnchor();
                     AnchorNode anchorNode = new AnchorNode(anchor);
                     anchorNode.setParent(arFragment.getArSceneView().getScene());
-                    TransformableNode lamp = new TransformableNode(arFragment.getTransformationSystem());
-                    lamp.setParent(anchorNode);
-                    lamp.setRenderable(lampPostRenderable);
-                    lamp.select();
-                });*/
 
+                    // Create the transformable andy and add it to the anchor.
+                    TransformableNode andy2 = new TransformableNode(arFragment.getTransformationSystem());
+                    andy2.setParent(anchorNode);
+                    andy2.setRenderable(andyRenderable);
+                    andy2.select();
+                });
+*/
 
+        arSceneView
+                .getScene()
+                .addOnUpdateListener(
+                        frameTime -> {
 
+                            if (locationScene == null) {
+                                locationScene = new LocationScene(this, arSceneView);
+                                locationScene.mLocationMarkers.add(
+                                        new LocationMarker(
+                                                18.5729,
+                                                54.3516,
+                                                getAndy()));
+                            }
+
+                            if (locationScene != null) {
+                                locationScene.processFrame(arFrame);
+                            }
+
+                        });
+
+    }
+
+    private Node getAndy() {
+        Node base = new Node();
+        base.setRenderable(andyRenderable );
+        Context c = this;
+        base.setOnTapListener((v, event) -> {
+            Toast.makeText(
+                    c, "Andy touched.", Toast.LENGTH_LONG)
+                    .show();
+        });
+        return base;
     }
 
     public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
