4e8d15c57feacd57061d8c12e0958fc94c60fd9b
Kasia <k.hudy96@gmail.com>
2020-06-04 17:24:10 +0200

    Project cleaning

--- app/src/main/java/com/example/sewersar/MainActivity.java
+++ app/src/main/java/com/example/sewersar/MainActivity.java
@@ -1,66 +1,37 @@
 package com.example.sewersar;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import com.example.sewersar.LocationMarker;
-//import uk.co.appoly.arcorelocation.LocationScene;
-//import uk.co.appoly.arcorelocation.rendering.LocationNode;
-//import uk.co.appoly.arcorelocation.rendering.LocationNodeRender;
-//import uk.co.appoly.arcorelocation.utils.ARLocationPermissionHelper;
 import com.example.sewersar.sensor.DeviceOrientation;
+import com.example.sewersar.utils.ARLocationPermissionHelper;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-
-import  android.app.Activity;
-import  android.app.ActivityManager;
-import 	android.os.Build;
-import 	android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.TextView;
-import 	android.widget.Toast;
-import 	android.content.Context;
+import android.widget.Toast;
+import android.content.Context;
 
-import com.example.sewersar.utils.ARLocationPermissionHelper;
-import com.google.ar.core.Anchor;
-import com.google.ar.core.ArCoreApk;
-import com.google.ar.core.Config;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.ar.core.Frame;
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
 import com.google.ar.core.Session;
 import com.google.ar.core.TrackingState;
 import com.google.ar.core.exceptions.CameraNotAvailableException;
 import com.google.ar.core.exceptions.UnavailableException;
-import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Color;
-import com.google.ar.sceneform.rendering.Material;
 import com.google.ar.sceneform.rendering.MaterialFactory;
-import  com.google.ar.sceneform.rendering.ModelRenderable;
-import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.ShapeFactory;
-import com.google.ar.sceneform.rendering.ViewRenderable;
-import  com.google.ar.sceneform.ux.ArFragment;
-import 	android.net.Uri;
-import 	android.view.Gravity;
 import com.google.ar.sceneform.ArSceneView;
-import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
 
     private boolean installRequested;
     private boolean hasFinishedLoading = false;
@@ -71,8 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
     private LocationScene locationScene;
 
-    private LocationManager locationManager;
-    private LocationListener locationListener;
 
     private ModelRenderable redSphereRenderable;
     private ModelRenderable pipe;
@@ -97,9 +66,6 @@ public class MainActivity extends AppCompatActivity {
             "FutureReturnValueIgnored"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //if (!checkIsSupportedDeviceOrFinish(this)) {
-            //return;
-       // }
         dOrientation = new DeviceOrientation(this);
         dOrientation.resume();
 
@@ -136,23 +102,11 @@ public class MainActivity extends AppCompatActivity {
                             return null;
                         });
 
-        /*locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new MyLocationListener(this,locationScene, andyRenderable, arSceneView);
-        if(ActivityCompat
-                .checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                PackageManager.PERMISSION_GRANTED) {
-            locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-        } else {
-            //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_LOCATION);
-        }*/
-
         MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
                 .thenAccept(
                         material -> {
                             redSphereRenderable =
                                     ShapeFactory.makeSphere(0.1f, new Vector3(0.0f, 0.00f, 0.0f), material); });
-
-
 
         arSceneView
                 .getScene()
@@ -167,9 +121,7 @@ public class MainActivity extends AppCompatActivity {
                                 // We know that here, the AR components have been initiated.
                                 locationScene = new LocationScene(this, arSceneView);
 
-
                                 // Adding a simple location marker of a 3D model
-
 
                                 locationScene.mLocationMarkers.add(
                                         new LocationMarker(
@@ -189,30 +141,7 @@ public class MainActivity extends AppCompatActivity {
                                                 points[2][1],
                                                 getAndy()));
 
-                                double newLon = (points[0][0] + points[2][0])/2;
-                                double newLat = (points[0][1] + points[2][1])/2;
-                                float newHeight = measure(points[0][1], points[0][0], points[2][1], points[2][0])/2;
-                                float newX = points[0][0] - points[2][0];
-                                float newY = points[0][1] - points[2][1];
-                                float beta = (float)(Math.atan(newY/newX) * 180/Math.PI);
-
-
-                                double lat1 = points[0][1];
-                                double lat2 = points[2][1];
-                                double dLon = Math.abs(points[0][0] - points[2][0]);
-                                double y2 = Math.sin(dLon) * Math.cos(lat2);
-                                double x2 = Math.cos(lat1)*Math.sin(lat2) -
-                                        Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
-                                double brng = Math.toDegrees(Math.atan2(y2, x2));
-                                float o = locationScene.deviceOrientation.getOrientation();
-                                double o2 = dOrientation.getOrientation();
-                                float angle = (float)(o2+brng+90.0);
-                                locationScene.mLocationMarkers.add(
-                                        new LocationMarker(
-                                                newLon,
-                                                newLat,
-                                                getPipe(newHeight, angle, locationScene)));
-                                int x = 0;
+                                addPipe(points[0][1], points[0][0], points[2][1], points[2][0]);
 
                             }
 
@@ -245,8 +174,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void addPipe(double lat1, double lon1, double lat2, double lon2) {
+        double newLon = (lon1 + lon2)/2;
+        double newLat = (lat1 + lat2)/2;
+        float newDistance = measureDistance(lat1, lon1, lat2, lon2)/3;
+        double bearing = countBearing(lat1, lon1, lat2, lon2);
+        double currentOrientation = dOrientation.getOrientation();
+        float angle = (float)(currentOrientation + bearing + 90.0);
+        locationScene.mLocationMarkers.add(
+                new LocationMarker(
+                        newLon,
+                        newLat,
+                        getPipe(newDistance, angle, locationScene)));
+    }
 
-    private float measure(float lat1, float lon1, float lat2, float lon2){  // generally used geo measurement function
+    private double countBearing(double lat1, double lon1, double lat2, double lon2) {
+        double dLon = Math.abs(lon1 - lon2);
+        double y2 = Math.sin(dLon) * Math.cos(lat2);
+        double x2 = Math.cos(lat1)*Math.sin(lat2) -
+                Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
+        double bearing = Math.toDegrees(Math.atan2(y2, x2));
+        return bearing;
+    }
+
+
+    private float measureDistance(double lat1, double lon1, double lat2, double lon2){  // generally used geo measurement function
         float R = 6378.137f; // Radius of earth in KM
         double dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
         double dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
